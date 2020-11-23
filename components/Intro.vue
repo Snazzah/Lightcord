21a4271be06cd6ef1bea245c09c753ea5b20c529
Snazzah <7025343+Snazzah@users.noreply.github.com>
2020-11-23 05:58:00 +0000

    Fix intro component

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,8 +1,8 @@
 <template lang="md">
-::: .warning **Lightcord does not send your bot's token or information to any
-3rd party.** Make sure that you trust the host of this site or that you are
-using the [official site for Lightcord](). :::
+  ::: .warning
+  **Lightcord does not send your bot's token or information to any 3rd party.**
+  Make sure that you trust the host of this site or that you are using the [official site for Lightcord]().
+  :::
 
-Lightcord is a webapp for Discord bots, allowing you to interact with Discord
-servers as the bot.
+  Lightcord is a webapp for Discord bots, allowing you to interact with Discord servers as the bot.
 </template>
