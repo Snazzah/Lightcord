41c1d5414e50c46e7324d5e96a0131cfc8e8e466
Snazzah <7025343+Snazzah@users.noreply.github.com>
2020-12-10 17:25:19 +0000

    Move suppress embed button to a new component

--- components/MessageEmbed.vue
+++ components/MessageEmbed.vue
@@ -47,19 +47,7 @@
     "
   >
     <div class="grid">
-      <div
-        class="suppress-button"
-        aria-label="Remove all embeds"
-        role="button"
-        tabindex="0"
-      >
-        <svg aria-hidden="false" width="16" height="16" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"
-          />
-        </svg>
-      </div>
+      <message-suppress-embeds />
       <div v-if="source.provider" class="embed-provider">
         <a
           v-if="source.provider.url"
@@ -232,19 +220,7 @@
     "
   >
     <div class="grid">
-      <div
-        class="suppress-button"
-        aria-label="Remove all embeds"
-        role="button"
-        tabindex="0"
-      >
-        <svg aria-hidden="false" width="16" height="16" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"
-          />
-        </svg>
-      </div>
+      <message-suppress-embeds />
       <div v-if="source.provider" class="embed-provider">
         <a
           v-if="source.provider.url"
---
+++ components/MessageSuppressEmbeds.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="suppress-button"
+    aria-label="Remove all embeds"
+    role="button"
+    tabindex="0"
+  >
+    <svg aria-hidden="false" width="16" height="16" viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"
+      />
+    </svg>
+  </div>
+</template>
